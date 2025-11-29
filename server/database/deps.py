d3eccfa54d8e5814e6bf26373e3a5860dcd231aa
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-11-29 13:28:16 +0545

    feat: dependency function for database ORM

---
+++ server/database/deps.py
@@ -0,0 +1,12 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
+from .database import AsyncSessionLocal
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit() 
+        except:
+            await session.rollback() 
+            raise
