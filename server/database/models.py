8fab288aef7216597316abd3a5a77ac804923613
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-11-29 17:58:53 +0545

    add: document model

--- server/database/models.py
+++ server/database/models.py
@@ -1,9 +1,23 @@
 import uuid
-from sqlalchemy import Column,UUID,String 
+from sqlalchemy import Column,UUID, Integer,String 
+
 from database.database import Base
+
+def uuid_no_dash():
+    return uuid.uuid4().hex   # removes dashes
+
+document_id = Column(String(32), primary_key=True, default=uuid_no_dash)
 class User(Base):
     __tablename__="user"
-    id=Column(UUID,primary_key=True,index=True,default=uuid.uuid4)
+    id=Column(UUID,primary_key=True,index=True,default=uuid_no_dash)
     username=Column(String,nullable=False,unique=True)
     password=Column(String,nullable=False)
-    
+
+class DocumentModel(Base):
+    __tablename__="documents"
+    document_id=Column(UUID,primary_key=True,index=True)
+    file_name=Column(String,nullable=False)
+    file_path=Column(String,nullable=False,unique=True)
+    file_size=Column(Integer,nullable=False)
+    upload_timestamp=Column(String,nullable=False)
+
