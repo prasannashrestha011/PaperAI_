6ed19a1482b775d7ae6cd52c715b55e7c512f767
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-11-29 18:52:56 +0545

    add: schemas for document

---
+++ server/schemas/document.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Optional
+from uuid import UUID
+
+class DocumentBase(BaseModel):
+    user_id: UUID
+    document_id:UUID
+    file_name: str
+    file_path: str
+    file_size: int
+    upload_timestamp: str  
+
+class DocumentCreate(DocumentBase):
+    pass  # all fields required for creation are already in DocumentBase
+
+class DocumentUpdate(BaseModel):
+    file_name: Optional[str] = None
+    file_path: Optional[str] = None
+    file_size: Optional[int] = None
+    upload_timestamp: Optional[str] = None
+
+class DocumentOut(DocumentBase):
+    class Config:
+        orm_mode = True
