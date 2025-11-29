8e0227704c26c2c8e24c78323e741c50d8593615
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-11-29 17:59:15 +0545

    add: middleware for app gateway

---
+++ server/middleware.py
@@ -0,0 +1,22 @@
+from fastapi import Request 
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi.responses import JSONResponse
+from utils.jwt import decode_access_token
+
+class JWTMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        protected_paths = ["/me", "/protected"]
+
+        if any(request.url.path.startswith(path) for path in protected_paths):
+            token = request.cookies.get("access_token")
+            if not token:
+                return JSONResponse({"detail": "Missing token"}, status_code=401)
+
+            payload = decode_access_token(token)
+            if not payload or "user_id" not in payload:
+                return JSONResponse({"detail": "Invalid or expired token"}, status_code=401)
+
+            request.state.user_id = payload["user_id"]
+
+        response = await call_next(request)
+        return response
