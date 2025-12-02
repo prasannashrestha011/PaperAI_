aeb3cccffe43da5f5aba09feb4a0abc382233190
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-02 21:07:44 +0545

    add: imported session_router

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from server.api.auth import auth_router
 from server.api.uploader import upload_router
+from server.api.agent_session import session_router 
 from fastapi.middleware.cors import CORSMiddleware
 from server.agent.graph_store import kg_store 
 origins = ["http://localhost:3000"]  # React dev server
