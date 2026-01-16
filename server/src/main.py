4d68044a83bbe58b6a339d629cca714bf328ea78
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-01-16 14:44:36 +0545

    temporarily suppressed routes in main

--- server/src/main.py
+++ server/src/main.py
@@ -1,11 +1,8 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from src.api.deep_agent import deep_agent_router 
-from src.api.auth import auth_router
 from src.api.uploader import upload_router
-from src.api.agent_session import session_router 
+from src.api.minio import router as pdf_router
 from fastapi.middleware.cors import CORSMiddleware
-# from src.agent.graph_store import kg_store 
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000"
@@ -29,7 +26,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(deep_agent_router)
-app.include_router(auth_router)
 app.include_router(upload_router)
-app.include_router(session_router)
+app.include_router(pdf_router)
