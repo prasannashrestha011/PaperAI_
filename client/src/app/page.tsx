953a8cde08f98b7941a459c710ce75f5f25e9a7f
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-01-19 15:58:27 +0545

    feat(ui): imported index page ui components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,14 @@
-import TestViewer from "@/components/PdfViewer"
-export default function Home() {
+"use client"
+import HeroSection from '@/components/HeroSection';
+import FeaturesSection from '@/components/FeaturesSection';
+import CursorLight from '@/components/CursorLight';
 
+export default function Home() {
   return (
-    <TestViewer/>
-  )
+    <div className="relative bg-slate-950">
+      <CursorLight/>
+      <HeroSection />
+      <FeaturesSection />
+    </div>
+  );
 }
